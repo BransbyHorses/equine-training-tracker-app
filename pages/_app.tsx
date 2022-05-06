3f3d8dc74ae37f0a1dc51b9b3840c0e9ad5e7886
michaelthorpe <michael.thorpe@digital.homeoffice.gov.uk>
2022-05-06 15:43:55 +0100

    remove next content and style files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import '../styles/globals.css'
 import { AppProps } from 'next/app'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
@@ -12,9 +11,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
           name="viewport"
           content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
         />
-        <meta name="description" content="Description" />
-        <meta name="keywords" content="Keywords" />
-        <title>Next.js PWA Example</title>
+        <title>Equine Training Tracker</title>
 
         <link rel="manifest" href="/manifest.json" />
         <link

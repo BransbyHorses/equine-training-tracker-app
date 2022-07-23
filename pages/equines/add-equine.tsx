a1ba2e0f67cedf6cad84a9059e9f57b587d92522
Hugo Jeffreys <hugo.jeffreys@digital.homeoffice.gov.uk>
2022-07-23 15:31:18 +0100

    Adding the form

--- pages/equines/add-equine.tsx
+++ pages/equines/add-equine.tsx
@@ -1,7 +1,15 @@
+import React, {useState, useContext} from 'react';
 import Link from 'next/link';
 
 export default function NewEquine (){
-
+    const [name, setName] = useState('');
+    const [yard, setYard] = useState('');
+    const [trainer, setTrainer] = useState(0);
+    const [category, setCategory] = useState('');
+    const [programme, setProgramme] = useState('');
+    const [skills, setSkills] = useState('');
+    const [training, setTraining] = useState('');
+    const [onHold, setOnHold] = useState(false);
 
     return(
         <div>
@@ -9,10 +17,10 @@ export default function NewEquine (){
             <div>
                 <form action="/data/equines" method='post' className='new_form'>
                     <label htmlFor="name">Name:</label>
-                    <input type="text" id='name' name='name'/>
+                    <input type="text" id='name' name='name' onChange={(e) => setName(e.target.value)}/>
 
                     <label htmlFor="equine_yard">Yard:</label>
-                    <select name="yard" id="equine_yard">
+                    <select name="yard" id="equine_yard" onChange={(e) => setYard(e.target.value)}>
                         <option value="yard 1">Yard 1</option>
                         <option value="yard 2">Yard 2</option>
                         <option value="yard 3">Yard 3</option>
@@ -21,10 +29,10 @@ export default function NewEquine (){
                     </select>
 
                     <label htmlFor="trainer-id"></label>
-                    <input type="number" id='trainer_id' min='1' max='20'/>
+                    <input type="number" id='trainer_id' min='1' max='20'onChange={(e) => setTrainer(parseInt(e.target.value))}/>
 
                     <label htmlFor="category">Category:</label>
-                    <select name="category" id="new_yard">
+                    <select name="category" id="new_yard" onChange={(e) => setCategory(e.target.value)} >
                         <option value="red">Red</option>
                         <option value="amber">Amber</option>
                         <option value="blue">Blue</option>
@@ -32,16 +40,16 @@ export default function NewEquine (){
                     </select>
 
                     <label htmlFor="programme">Programme:</label>
-                    <input type="text" id='programme' name='programme' />
+                    <input type="text" id='programme' name='programme' onChange={(e) => setProgramme(e.target.value)}/>
 
                     <label htmlFor="skills">Skills:</label>
-                    <input type="text" id='skills' name='skills' />
+                    <input type="text" id='skills' name='skills' onChange={(e) => setSkills(e.target.value)} />
 
                     <label htmlFor="training">Training:</label>
-                    <input type="text" id='training' name='training' />
+                    <input type="text" id='training' name='training' onChange={(e) => setTrainer(parseInt(e.target.value))} />
 
                     <label htmlFor="on_hold">On Hold:</label>
-                    <input type="checkbox" />
+                    <input type="checkbox" onChange={(e) => setOnHold(e.target.checked)}/>
                 </form>
             </div>
             <div>

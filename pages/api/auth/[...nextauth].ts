083b975a3b27d7061a114f43a5f556dfc1383db8
michaelthorpe <michael.thorpe@digital.homeoffice.gov.uk>
2022-09-28 15:43:16 +0100

    add authentication on all routes

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,41 +1,35 @@
-// import NextAuth from "next-auth"
-// import CognitoProvider from "next-auth/providers/cognito"
-// import jwt_decode from 'jwt-decode'
+import NextAuth from "next-auth"
+import CognitoProvider from "next-auth/providers/cognito"
+import jwt_decode from 'jwt-decode'
 
-// export default NextAuth({
-//   // Configure one or more authentication providers
-//   providers: [
-//     CognitoProvider({
-//       clientId: process.env.NEXT_PUBLIC_COGNITO_CLIENT_ID,
-//       clientSecret: process.env.NEXT_PUBLIC_COGNITO_CLIENT_SECRET,
-//       issuer: process.env.NEXT_PUBLIC_COGNITO_ISSUER
-//     }),
-//   ],
+export default NextAuth({
+  providers: [
+    CognitoProvider({
+      clientId: process.env.NEXT_PUBLIC_COGNITO_CLIENT_ID,
+      clientSecret: process.env.NEXT_PUBLIC_COGNITO_CLIENT_SECRET,
+      issuer: process.env.NEXT_PUBLIC_COGNITO_ISSUER
+    }),
+  ],
 
-//   session: {
-//     strategy: 'jwt',
-//   },
+  session: {
+    strategy: 'jwt',
+  },
 
-//   callbacks: {
-//     async jwt({ token, account }) {
-//       if (account) {
-//         // console.log(token);
-//         // console.log(account);
-        
-//         token.accessToken = account.access_token;
-//         let decodedToken: string = jwt_decode(account.access_token!);
-//         token.role = decodedToken["cognito:groups"] ? decodedToken["cognito:groups"][0] : null;
-//         token.sub = decodedToken['sub'];
-//       }
-//       return token;
-//     },
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token;
+        let decodedToken: string = jwt_decode(account.access_token!);
+        token.role = decodedToken["cognito:groups"] ? decodedToken["cognito:groups"][0] : null;
+        token.sub = decodedToken['sub'];
+      }
+      return token;
+    },
 
-//     async session({ session, token }){
-      
-//       session.role = token.role;
-//       session.userId = token.sub;
-//       // console.log(session);
-//       return session;
-//     }
-//   }
-// })
+    async session({ session, token }){
+      session.role = token.role;
+      session.userId = token.sub;
+      return session;
+    }
+  }
+})

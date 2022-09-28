67e4af33dd90e06b342185b4a9804d2694668c1d
michaelthorpe <michael.thorpe@digital.homeoffice.gov.uk>
2022-09-28 16:33:36 +0100

    structure navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,59 +1,45 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import { signIn, signOut, useSession } from 'next-auth/react';
-import HomeIcon from './HomeIcon';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import { signIn, signOut, useSession } from "next-auth/react";
+import HomeIcon from "./HomeIcon";
 
 export default function ButtonAppBar() {
-    const { data: session, status } = useSession();
-    const loading = status === 'loading';
+	const { data: session, status } = useSession();
+	const loading = status === "loading";
 
-    const signInUser = () => {
-        signIn('cognito', {
-            callbackUrl: `${window.location.origin}/equines`
-        });
-    };
+	const signInUser = () => {
+		signIn("cognito", {
+			callbackUrl: `${window.location.origin}/equines`,
+		});
+	};
 
-    const signOutUser = () => {
-        signOut({
-            callbackUrl: `${window.location.origin}/`
-        });
-    };
+	const signOutUser = () => {
+		signOut({
+			callbackUrl: `${window.location.origin}/`,
+		});
+	};
 
-    return (
-        <Box sx={{ flexGrow: 1 }}>
-            <AppBar position="static">
-                <Toolbar sx={{
-                    flexDirection: "row",
-                    justifyContent: "center"
-                }}>
-                    <MenuIcon sx={{ height: '2.1rem', width: '2.1rem', display: "none"}}/>
-                    <IconButton
-                        size="small"
-                        edge="start"
-                        color="inherit"
-                        aria-label="menu"
-                    >
-                        <HomeIcon />
-                    </IconButton>
-
-                    {/* {!session && (
-                        <Button color="inherit" onClick={signInUser}>
-                            Login
-                        </Button>
-                    )}
-                    {session && (
-                        <Button color="inherit" onClick={signOutUser}>
-                            Sign Out
-                        </Button>
-                    )} */}
-                </Toolbar>
-            </AppBar>
-        </Box>
-    );
+	return (
+		<Box sx={{ flexGrow: 1 }}>
+			<AppBar position="static">
+				<Toolbar>
+					<MenuIcon sx={{ height: "2.1rem", width: "2.1rem" }} />
+					<IconButton
+						size="small"
+						color="inherit"
+						aria-label="menu"
+						sx={{ margin: "0 auto" }}
+					>
+						<HomeIcon />
+					</IconButton>
+				</Toolbar>
+			</AppBar>
+		</Box>
+	);
 }

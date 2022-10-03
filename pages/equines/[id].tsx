73701acf027d542890f8dfc6b2fdc4a077117899
michaelthorpe <michael.thorpe@digital.homeoffice.gov.uk>
2022-10-03 15:56:25 +0100

    add accordion to equine profile

--- pages/equines/[id].tsx
+++ pages/equines/[id].tsx
@@ -12,10 +12,15 @@ import {
 	Paper,
 	Grid,
 	styled,
+	Accordion,
+	AccordionSummary,
+	AccordionDetails,
 } from "@mui/material";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
 import ArrowRightIcon from "@mui/icons-material/ArrowRight";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import ReportProblemIcon from "@mui/icons-material/ReportProblem";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 import LoadingSpinner from "../../components/LoadingSpinner";
 import { findLatestTrainingProgramme } from "../../utils/helpers";
@@ -23,11 +28,12 @@ import { TrainingProgramme } from "../../utils/types";
 
 const EquineProfile = () => {
 	const router = useRouter();
-	const [equineId, setEquineId] = useState(undefined);
+	const [equineId, setEquineId] = useState<string | undefined>(undefined);
 	const { fetchingData, equine, error, notFound } = useEquine(
-		equineId,
-		router.isReady
+		router.isReady,
+		equineId
 	);
+	const [showEquineData, setShowEquineData] = useState(true);
 
 	useEffect(() => {
 		if (router.isReady) {
@@ -70,7 +76,67 @@ const EquineProfile = () => {
 				</Link>
 				<Typography color="text.primary">{equine?.name}</Typography>
 			</Breadcrumbs>
-			<Paper>
+			<Accordion expanded={showEquineData} onChange={() => setShowEquineData(!showEquineData)}>
+				<Box
+					mt={2}
+					color="common.white"
+					sx={{ backgroundColor: "primary.light" }}
+				>
+					<AccordionSummary expandIcon={<ExpandMoreIcon fontSize="large" />}>
+						<Typography variant="h4">{equine?.name}</Typography>
+					</AccordionSummary>
+				</Box>
+				<AccordionDetails>
+					<Box sx={{ flexGrow: 1, backgroundColor: "common.white" }}>
+						<Grid container>
+							<Grid xs={12} md={6}>
+								<Item>
+									<Typography variant="h6">Training Programme</Typography>
+									{equine && currentTrainingProgramme ? (
+										<Typography>
+											{currentTrainingProgramme.trainingCategory.name}
+										</Typography>
+									) : (
+										<MoreHorizIcon sx={{ color: "gray" }} />
+									)}
+								</Item>
+							</Grid>
+							<Grid xs={12} md={6}>
+								<Item>
+									<Typography variant="h6">Current Yard</Typography>
+									{equine && equine.yard ? (
+										<Typography>{equine.yard.name}</Typography>
+									) : (
+										<MoreHorizIcon sx={{ color: "gray" }} />
+									)}
+								</Item>
+							</Grid>
+							<Grid xs={12} md={6}>
+								<Item>
+									<Typography variant="h6">Status</Typography>
+									{equine && equine.equineStatus ? (
+										<Typography>{equine.equineStatus.name}</Typography>
+									) : (
+										<MoreHorizIcon sx={{ color: "gray" }} />
+									)}
+								</Item>
+							</Grid>
+							<Grid xs={12} md={6}>
+								<Item>
+									<Typography variant="h6">Type of learner</Typography>
+									{equine && equine.learnerType ? (
+										<Typography>{equine.learnerType.name}</Typography>
+									) : (
+										<MoreHorizIcon sx={{ color: "gray" }} />
+									)}
+								</Item>
+							</Grid>
+						</Grid>
+					</Box>
+				</AccordionDetails>
+			</Accordion>
+
+			{/* <Paper>
 				<Box
 					p={1}
 					mt={2}
@@ -125,7 +191,7 @@ const EquineProfile = () => {
 						</Grid>
 					</Grid>
 				</Box>
-			</Paper>
+			</Paper> */}
 			<hr style={{ margin: "20px 0" }} />
 			<Grid container rowSpacing={3} columnSpacing={2}>
 				<Grid item xs={12} sm={6}>
@@ -172,6 +238,33 @@ const EquineProfile = () => {
 							}}
 						>
 							<Typography variant="h6">Training History</Typography>
+							<ArrowRightIcon fontSize="large" />
+						</Box>
+					</Paper>
+				</Grid>
+				<Grid item xs={12} sm={6}>
+					<Paper>
+						<Box
+							px={2}
+							py={2}
+							sx={{
+								display: "flex",
+								justifyContent: "space-between",
+								alignItems: "center",
+							}}
+						>
+							<Box
+								sx={{
+									display: "flex",
+									justifyContent: "space-between",
+									alignItems: "center",
+								}}
+							>
+								<ReportProblemIcon fontSize="medium" />
+								<Typography sx={{ ml: 1 }} variant="h6">
+									Health & Safety
+								</Typography>
+							</Box>
 							<ArrowRightIcon fontSize="large" />
 						</Box>
 					</Paper>

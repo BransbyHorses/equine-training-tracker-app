d18d14b8ded80b5d676f49d5481c28262de0246a
michaelthorpe <michael.thorpe@digital.homeoffice.gov.uk>
2022-10-07 13:49:00 +0100

    training programme tables

---
+++ components/pages/equines/TrainingProgrammesTable.tsx
@@ -0,0 +1,130 @@
+import React from "react";
+
+import { TrainingProgramme } from "../../../utils/types";
+
+import {
+	TableContainer,
+	TableHead,
+	Table,
+	TableCell,
+	TableRow,
+	TableBody,
+	Paper,
+	Typography,
+} from "@mui/material";
+import {
+	convertDateToString,
+	findCurrentTrainingProgramme,
+	findLastTrainingSession,
+} from "../../../utils/helpers";
+
+const TrainingProgrammesTable = ({
+	trainingProgrammes,
+	data,
+}: {
+	trainingProgrammes: TrainingProgramme[];
+	data: "active" | "inactive";
+}) => {
+	const mapTableRows = (trainingProgrammes: TrainingProgramme[]) => {
+		if (data === "active") {
+			return trainingProgrammes.map((trainingProgramme) => {
+				const lastDateTrainedValue = findLastTrainingSession(
+					trainingProgramme.skillTrainingSessions
+				);
+
+				return (
+					<TableRow key={trainingProgramme.id}>
+						<TableCell>{trainingProgramme.trainingCategory.name}</TableCell>
+						<TableCell>
+							{trainingProgramme.startDate ? (
+								convertDateToString(trainingProgramme.startDate)
+							) : (
+								<em>Not started</em>
+							)}
+						</TableCell>
+						<TableCell>
+							{!trainingProgramme.startDate ? (
+								<em>Not started</em>
+							) : (
+								convertDateToString(lastDateTrainedValue)
+							)}
+						</TableCell>
+						<TableCell>
+							{trainingProgramme.skillProgressRecords.length}
+						</TableCell>
+						<TableCell>
+							{trainingProgramme.skillTrainingSessions.length}
+						</TableCell>
+					</TableRow>
+				);
+			});
+		}
+
+		if (data === "inactive") {
+			return trainingProgrammes.map((trainingProgramme, index) => {
+				return (
+					<TableRow key={trainingProgramme.id}>
+						<TableCell>{trainingProgramme.trainingCategory.name}</TableCell>
+						<TableCell>
+							{convertDateToString(trainingProgramme.startDate)}
+						</TableCell>
+						<TableCell>
+							{convertDateToString(trainingProgramme.endDate)}
+						</TableCell>
+						<TableCell>
+							{trainingProgramme.skillProgressRecords.length}
+						</TableCell>
+						<TableCell>
+							{trainingProgramme.skillTrainingSessions.length}
+						</TableCell>
+					</TableRow>
+				);
+			});
+		}
+	};
+
+	const mapTableHead = () => {
+		if (data === "active") {
+			return (
+				<>
+					<TableCell>Training Category</TableCell>
+					<TableCell>Start Date</TableCell>
+					<TableCell>Last Date Trained</TableCell>
+					<TableCell>Skills in Training</TableCell>
+					<TableCell>Training Sessions</TableCell>
+				</>
+			);
+		}
+
+		if (data === "inactive") {
+			return (
+				<>
+					<TableCell>Training Category</TableCell>
+					<TableCell>Start Date</TableCell>
+					<TableCell>End Date</TableCell>
+					<TableCell>Skills Trained</TableCell>
+					<TableCell>Training Sessions</TableCell>
+				</>
+			);
+		}
+	};
+
+	return (
+		<>
+			{trainingProgrammes.length === 0 ? (
+				<Typography p={1}>
+					<em>No training programmes to display</em>
+				</Typography>
+			) : (
+				<TableContainer component={Paper}>
+					<Table>
+						<TableHead>{mapTableHead()}</TableHead>
+						<TableBody>{mapTableRows(trainingProgrammes)}</TableBody>
+					</Table>
+				</TableContainer>
+			)}
+		</>
+	);
+};
+
+export default TrainingProgrammesTable;
--- pages/equines/[equineId]/training-programmes/index.tsx
+++ pages/equines/[equineId]/training-programmes/index.tsx
@@ -3,19 +3,18 @@ import { useRouter } from "next/router";
 
 import useTrainingProgrammes from "../../../../utils/hooks/useTrainingProgrammes";
 
-import { Breadcrumbs, Link, Box, Alert } from "@mui/material";
+import { Breadcrumbs, Link, Box, Alert, Typography } from "@mui/material";
 import LoadingSpinner from "../../../../components/LoadingSpinner";
 import ArrowLeftIcon from "@mui/icons-material/ArrowLeft";
+import TrainingProgrammesTable from "../../../../components/pages/equines/TrainingProgrammesTable";
 
 const EquineTrainingProgrammes = () => {
 	const router = useRouter();
 	const [equineId, setEquineId] = useState<string | undefined>(undefined);
-	const { fetchingData, trainingProgrammes, error } = useTrainingProgrammes(
-		equineId
-	);
+	const { fetchingData, trainingProgrammes, error } =
+		useTrainingProgrammes(equineId);
 
 	useEffect(() => {
-		console.log(router.query.equineId);
 		if (router.isReady) {
 			setEquineId(router.query["equineId"]);
 		}
@@ -39,6 +38,8 @@ const EquineTrainingProgrammes = () => {
 		);
 	}
 
+	console.log(trainingProgrammes);
+
 	return (
 		<>
 			<Breadcrumbs>
@@ -51,7 +52,7 @@ const EquineTrainingProgrammes = () => {
 					<ArrowLeftIcon /> Back
 				</Link>
 			</Breadcrumbs>
-			{trainingProgrammes.length === 0 && (
+			{trainingProgrammes.length === 0 ? (
 				<Box
 					mt={3}
 					sx={{
@@ -61,6 +62,24 @@ const EquineTrainingProgrammes = () => {
 					}}
 				>
 					<Alert severity="info">No Training History Available</Alert>
+				</Box>
+			) : (
+				<Box mt={3}>
+					<Typography variant="h6" mb={2} color="gray">
+						Active Training Programmes
+					</Typography>
+					<TrainingProgrammesTable
+						trainingProgrammes={trainingProgrammes.filter((t) => !t.endDate)}
+						data="active"
+					/>
+					<hr style={{ margin: "20px 0" }} />
+					<Typography variant="h6" mb={2} mt={5} color="gray">
+						Completed Training Programmes
+					</Typography>
+					<TrainingProgrammesTable
+						trainingProgrammes={trainingProgrammes.filter((t) => t.endDate)}
+						data="inactive"
+					/>
 				</Box>
 			)}
 		</>

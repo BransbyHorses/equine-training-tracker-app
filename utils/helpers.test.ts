9b6911815388eec331ba636dcf2039f00caf1266
michaelthorpe <michael.thorpe@digital.homeoffice.gov.uk>
2022-10-07 13:48:41 +0100

    find last training session with test coverage

--- utils/helpers.test.ts
+++ utils/helpers.test.ts
@@ -1,5 +1,9 @@
-import { findLatestTrainingProgramme } from "./helpers";
-import { TrainingProgramme } from "./types";
+import {
+	findCurrentTrainingProgramme,
+	convertDateToString,
+	findLastTrainingSession,
+} from "./helpers";
+import { SkillTrainingSession, TrainingProgramme } from "./types";
 
 describe("find latest training programme in array", () => {
 	test("returns only training programme without an end date", () => {
@@ -24,7 +28,7 @@ describe("find latest training programme in array", () => {
 			},
 		];
 
-		const result = findLatestTrainingProgramme(trainingProgrammes);
+		const result = findCurrentTrainingProgramme(trainingProgrammes);
 		expect(result).toBe(trainingProgrammes[0]);
 	});
 	test("returns null if all training programmes have an end date", () => {
@@ -48,11 +52,53 @@ describe("find latest training programme in array", () => {
 				endDate: "2022-10-01",
 			},
 		];
-		const result = findLatestTrainingProgramme(trainingProgrammes);
+		const result = findCurrentTrainingProgramme(trainingProgrammes);
 		expect(result).toBeNull();
 	});
 	test("returns null if input array is empty", () => {
-		const result = findLatestTrainingProgramme([]);
+		const result = findCurrentTrainingProgramme([]);
 		expect(result).toBeNull();
+	});
+});
+
+describe("print date to user friendly steing", () => {
+	test("correctly prints string", () => {
+		const stringDate = convertDateToString("2022-10-15");
+		expect(stringDate).toBe("15 October 2022");
+	});
+	test("returns empty string if value is falsy", () => {
+		const stringDate = convertDateToString(undefined);
+		expect(stringDate).toBe("");
+	});
+});
+
+describe("find the last training session", () => {
+	test("returns last training session based on date", () => {
+		const skillTrainingSessions = [
+			{
+				id: 1,
+				date: "2022-08-15",
+			},
+			{
+				id: 1,
+				date: "2022-06-15",
+			},
+			{
+				id: 1,
+				date: "2022-09-15",
+			},
+			{
+				id: 1,
+				date: "2022-07-15",
+			},
+		];
+		const latestTrainingSession = findLastTrainingSession(
+			skillTrainingSessions
+		);
+		expect(latestTrainingSession).toBe("2022-09-15");
+	});
+	test("returns null is argument is falsy", () => {
+		const v = findLastTrainingSession(null);
+		expect(v).toBeNull();
 	});
 });

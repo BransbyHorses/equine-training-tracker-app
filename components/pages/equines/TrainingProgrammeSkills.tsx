55696f27bceff9a56505fa42f97b1b16110d1c01
michaelthorpe <michael.thorpe@digital.homeoffice.gov.uk>
2022-10-11 12:55:46 +0100

    order skills aplphabetically

--- components/pages/equines/TrainingProgrammeSkills.tsx
+++ components/pages/equines/TrainingProgrammeSkills.tsx
@@ -62,91 +62,97 @@ const TrainingProgrammeSkills = ({
 	};
 
 	const mapSkillProgressRecords = () => {
-		return trainingProgrammeSkills?.map((skillProgessRecord, i) => {
-			const sortedSkillTrainingSessions = skillTrainingSessions
-				.filter(
-					(skillTrainingSession) =>
-						skillTrainingSession.skill.id === skillProgessRecord.skill.id
-				)
-				.sort((a, b) => {
-					new Date(b.date) - new Date(a.date);
-				});
+		return trainingProgrammeSkills
+			?.sort((a, b) => {
+				if (a.skill.name < b.skill.name) return -1;
+				if (a.skill.name > b.skill.name) return 1;
+				return 0;
+			})
+			.map((skillProgessRecord, i) => {
+				const sortedSkillTrainingSessions = skillTrainingSessions
+					.filter(
+						(skillTrainingSession) =>
+							skillTrainingSession.skill.id === skillProgessRecord.skill.id
+					)
+					.sort((a, b) => {
+						new Date(b.date) - new Date(a.date);
+					});
 
-			const lastTrainingSession =
-				sortedSkillTrainingSessions.length === 0 ? (
-					<span>No training sessions</span>
-				) : (
-					`Last trained on ${convertDateToString(
-						sortedSkillTrainingSessions[0].date
-					)}`
-				);
+				const lastTrainingSession =
+					sortedSkillTrainingSessions.length === 0 ? (
+						<span>No training sessions</span>
+					) : (
+						`Last trained on ${convertDateToString(
+							sortedSkillTrainingSessions[0].date
+						)}`
+					);
 
-			return (
-				<Paper key={skillProgessRecord.id}>
-					<Box
-						p={2}
-						mt={2}
-						sx={{
-							display: "flex",
-							justifyContent: "space-between",
-							alignItems: "center",
-						}}
-					>
-						<Box>
-							<Box
-								sx={{
-									display: "flex",
-									[theme.breakpoints.between("xs", "sm")]: {
-										flexDirection: "column",
-									},
-								}}
-							>
-								<Typography fontWeight={600}>
-									{skillProgessRecord.skill.name}
-								</Typography>
+				return (
+					<Paper key={skillProgessRecord.id}>
+						<Box
+							p={2}
+							mt={2}
+							sx={{
+								display: "flex",
+								justifyContent: "space-between",
+								alignItems: "center",
+							}}
+						>
+							<Box>
 								<Box
 									sx={{
+										display: "flex",
 										[theme.breakpoints.between("xs", "sm")]: {
-											margin: "8px 0",
-										},
-										[theme.breakpoints.between("sm", "xl")]: {
-											margin: "0 0 0 16px",
+											flexDirection: "column",
 										},
 									}}
 								>
-									<div
-										style={{
-											width: "fit-content",
-											padding: "2px 10px",
-											textAlign: "center",
-											backgroundColor:
-												progressTagPalette[
-													skillProgessRecord.progressCode.string
-												],
-											color:
-												skillProgessRecord.progressCode.string ===
-													ProgressCode["Not able"] ||
-												skillProgessRecord.progressCode.string ===
-													ProgressCode["Just started"]
-													? "black"
-													: "white",
+									<Typography fontWeight={600}>
+										{skillProgessRecord.skill.name}
+									</Typography>
+									<Box
+										sx={{
+											[theme.breakpoints.between("xs", "sm")]: {
+												margin: "8px 0",
+											},
+											[theme.breakpoints.between("sm", "xl")]: {
+												margin: "0 0 0 16px",
+											},
 										}}
 									>
-										<Typography fontWeight={500}>
-											{skillProgessRecord.progressCode.string}
-										</Typography>
-									</div>
+										<div
+											style={{
+												width: "fit-content",
+												padding: "2px 10px",
+												textAlign: "center",
+												backgroundColor:
+													progressTagPalette[
+														skillProgessRecord.progressCode.string
+													],
+												color:
+													skillProgessRecord.progressCode.string ===
+														ProgressCode["Not able"] ||
+													skillProgessRecord.progressCode.string ===
+														ProgressCode["Just started"]
+														? "black"
+														: "white",
+											}}
+										>
+											<Typography fontWeight={500}>
+												{skillProgessRecord.progressCode.string}
+											</Typography>
+										</div>
+									</Box>
 								</Box>
+								<Typography color="gray">
+									<small>{lastTrainingSession}</small>
+								</Typography>
 							</Box>
-							<Typography color="gray">
-								<small>{lastTrainingSession}</small>
-							</Typography>
+							<KeyboardArrowRightIcon fontSize="large" />
 						</Box>
-						<KeyboardArrowRightIcon fontSize="large" />
-					</Box>
-				</Paper>
-			);
-		});
+					</Paper>
+				);
+			});
 	};
 
 	return (

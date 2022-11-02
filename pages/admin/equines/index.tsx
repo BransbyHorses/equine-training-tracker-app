ba67d338753f323a629b842422e377bfe93890ae
michaelthorpe <michael.thorpe@digital.homeoffice.gov.uk>
2022-11-02 15:34:46 +0000

    edit add equine link

--- pages/admin/equines/index.tsx
+++ pages/admin/equines/index.tsx
@@ -30,7 +30,7 @@ export default function Equines() {
 		<>
 			<AdminPageTitle
 				title="Manage Equines"
-				buttonLink="/admin/equines/add/equine"
+				buttonLink="/admin/equines/add-equine"
 				contentLength={equines.length}
 			/>
 			{equines.length > 0 && (

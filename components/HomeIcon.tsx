6dfb6bc853d3b90c7412fbcc2c38be9ccda5737e
michaelthorpe <michael.thorpe@digital.homeoffice.gov.uk>
2022-11-06 21:10:52 +0000

    add priority to logo

--- components/HomeIcon.tsx
+++ components/HomeIcon.tsx
@@ -1,20 +1,16 @@
-import React from 'react';
-import Link from 'next/link';
-import Image from 'next/image';
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
 
-import NavLogoContainer from './styles/navLogoContainer';
-import bransbyLog from '../assets/svg/bransbyLogo.svg';
-
+import NavLogoContainer from "./styles/navLogoContainer";
+import bransbyLog from "../assets/svg/bransbyLogo.svg";
 
 const HomeIcon = () => {
-    return (
-        <NavLogoContainer>
-            <Image
-                src={bransbyLog}
-                layout="fill"
-            />
-        </NavLogoContainer>
-    );
-}
+	return (
+		<NavLogoContainer>
+			<Image src={bransbyLog} layout="fill" priority />
+		</NavLogoContainer>
+	);
+};
 
 export default HomeIcon;

0b481765e58479482de241eac2649a529c59f347
michaelthorpe <michael.thorpe@digital.homeoffice.gov.uk>
2022-11-06 17:10:17 +0000

    add onclick to breadcrumb

--- components/BackBreadcrumb.tsx
+++ components/BackBreadcrumb.tsx
@@ -1,23 +1,28 @@
 import React from "react";
-
 import { useRouter } from "next/router";
 
-import { Breadcrumbs, Link } from "@mui/material";
+import { Link } from "@mui/material";
 import ArrowLeftIcon from "@mui/icons-material/ArrowLeft";
 
-const BackBreadcrumb = ({ link }: { link?: string }) => {
+const BackBreadcrumb = ({
+	link,
+	onClick,
+}: {
+	link?: string;
+	onClick?: () => void;
+}) => {
 	const router = useRouter();
 	return (
-		<Breadcrumbs sx={{ mb: 2 }}>
-			<Link
-				underline="hover"
-				color="inherit"
-				sx={{ display: "flex", alignItems: "center", cursor: "pointer" }}
-				onClick={link ? () => router.push(link) : () => router.back()}
-			>
-				<ArrowLeftIcon /> Back
-			</Link>
-		</Breadcrumbs>
+		<Link
+			underline="hover"
+			color="inherit"
+			sx={{ display: "flex", alignItems: "center", cursor: "pointer" }}
+			onClick={
+				link ? () => router.push(link) : onClick ? onClick : () => router.back()
+			}
+		>
+			<ArrowLeftIcon /> Back
+		</Link>
 	);
 };
 

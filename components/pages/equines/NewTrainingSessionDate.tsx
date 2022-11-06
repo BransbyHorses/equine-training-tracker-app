4c9f33ac83b68cffe94021e45395d43c7939d4bb
michaelthorpe <michael.thorpe@digital.homeoffice.gov.uk>
2022-11-06 17:10:38 +0000

    add next and back actions to date picker component

--- components/pages/equines/NewTrainingSessionDate.tsx
+++ components/pages/equines/NewTrainingSessionDate.tsx
@@ -1,21 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import {
 	NewTrainingSessionContext,
 	NewSkillTrainingSessionType,
 } from "../../../utils/reducers/trainingSessionReducer";
-import BackBreadcrumb from "../../BackBreadcrumb";
+
 import ResponsiveButton from "../../ResponsiveButton";
+import BackBreadcrumb from "../../BackBreadcrumb";
+import PageTitle from "../../PageTitle";
 
 const NewTrainingSessionDate = () => {
-	const { state, dispatch } = React.useContext(NewTrainingSessionContext);
-
-	console.log(state);
+	const {
+		state: { newTrainingSession },
+		dispatch,
+	} = React.useContext(NewTrainingSessionContext);
+	const [date, setDate] = useState(newTrainingSession.date || "");
 
 	return (
 		<>
-			<BackBreadcrumb />
+			<BackBreadcrumb
+				onClick={() => dispatch({ type: NewSkillTrainingSessionType.BACK })}
+			/>
+			<PageTitle title="What date was your training session?" />
 			<ResponsiveButton
-				onClick={() => dispatch({ type: NewSkillTrainingSessionType.NEXT, payload:  })}
+				disabled={date}
+				onClick={() => dispatch({ type: NewSkillTrainingSessionType.NEXT })}
 			>
 				Continue
 			</ResponsiveButton>

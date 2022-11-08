c5a6100ebeccd0f11531b2ccbf7a2aa7cf913d70
michaelthorpe <michael.thorpe@digital.homeoffice.gov.uk>
2022-11-08 15:07:27 +0000

    replace styled components with mui styles

--- components/styles/input.tsx
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-const Input = styled.input`
-        height: 30px;
-    `
-
-export default Input;
--- components/styles/navLogoContainer.tsx
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-const NavLogoContainer = styled.div`
-        width: 120px;
-        height: 50px;
-    `;
-
-export default NavLogoContainer;

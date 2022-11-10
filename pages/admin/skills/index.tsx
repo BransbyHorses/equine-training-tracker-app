0885afc62bef540511491760420b890bd4c351cb
michaelthorpe <michael.thorpe@digital.homeoffice.gov.uk>
2022-11-10 13:37:08 +0000

    check statusText on admin delete function

--- pages/admin/skills/index.tsx
+++ pages/admin/skills/index.tsx
@@ -5,82 +5,84 @@ import { Skill } from "../../../utils/types";
 import axios from "axios";
 
 export default function SkillsAdminPage() {
-	const [skills, setSkills] = useState<Skill[]>([]);
-	const [editValue, setEditValue] = useState<Skill>();
+  const [skills, setSkills] = useState<Skill[]>([]);
+  const [editValue, setEditValue] = useState<Skill>();
 
-	function getSkills() {
-		fetch(`${process.env.NEXT_PUBLIC_URL}/data/skills`)
-			.then((response) => response.json())
-			.then((data) => setSkills(data))
-			.catch((rejected) => {
-				console.log(rejected);
-			});
-	}
+  function getSkills() {
+    fetch(`${process.env.NEXT_PUBLIC_URL}/data/skills`)
+      .then((response) => response.json())
+      .then((data) => setSkills(data))
+      .catch((rejected) => {
+        console.log(rejected);
+      });
+  }
 
-	useEffect(() => {
-		getSkills();
-	}, []);
+  useEffect(() => {
+    getSkills();
+  }, []);
 
-	const editSkill = (id: any) => {
-		setEditValue(skills.find((skill) => skill.id === id));
-	};
+  const editSkill = (id: any) => {
+    setEditValue(skills.find((skill) => skill.id === id));
+  };
 
-	const saveEditedSkill = (newSkillName: string) => {
-		if (newSkillName === editValue!.name) {
-			setEditValue(undefined);
-			return;
-		}
+  const saveEditedSkill = (newSkillName: string) => {
+    if (newSkillName === editValue!.name) {
+      setEditValue(undefined);
+      return;
+    }
 
-		const editedSkill = { id: editValue!.id, name: newSkillName };
+    const editedSkill = { id: editValue!.id, name: newSkillName };
 
-		axios
-			.put(
-				`${process.env.NEXT_PUBLIC_URL}data/skills/${editValue!.id}`,
-				editedSkill
-			)
-			.then(({ data }) => {
-				setEditValue(undefined);
-				const updatedSkills = skills.map((skill) => {
-					return skill.id === data.id ? data : skill;
-				});
-				setSkills(updatedSkills);
-			})
-			.catch((err) => {
-				console.error(err);
-			});
-	};
+    axios
+      .put(
+        `${process.env.NEXT_PUBLIC_URL}data/skills/${editValue!.id}`,
+        editedSkill
+      )
+      .then(({ data }) => {
+        setEditValue(undefined);
+        const updatedSkills = skills.map((skill) => {
+          return skill.id === data.id ? data : skill;
+        });
+        setSkills(updatedSkills);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
 
-	const deleteSkill = (id: any) => {
-		fetch(`${process.env.NEXT_PUBLIC_URL}/data/skills/${id}`, {
-			method: "DELETE",
-		})
-			.then(() => {
-				setSkills(skills.filter((skill) => skill.id != id));
-			})
-			.catch((rejected) => {
-				console.error(rejected);
-			});
-	};
+  const deleteSkill = (id: any) => {
+    fetch(`${process.env.NEXT_PUBLIC_URL}/data/skills/${id}`, {
+      method: "DELETE",
+    })
+      .then(({ statusText }) => {
+        if (statusText === "OK") {
+          setSkills(skills.filter((skill) => skill.id != id));
+        }
+      })
+      .catch((rejected) => {
+        console.error(rejected);
+      });
+  };
 
-	return (
-		<>
-			<AdminPageTitle
-				title="Manage Skills"
-				buttonLink="/admin/skills/add-skill"
-				contentLength={skills.length}
-			/>
-			{skills.map((skill) => {
-				return (
-					<AdminContentBlock
-						key={skill.id}
-						content={skill}
-						editValue={editValue}
-						saveFunction={saveEditedSkill}
-						editAction={editSkill}
-						deleteFunction={deleteSkill}
-					/>
-				);
-			})}
-		</>
-	);
+  return (
+    <>
+      <AdminPageTitle
+        title="Manage Skills"
+        buttonLink="/admin/skills/add-skill"
+        contentLength={skills.length}
+      />
+      {skills.map((skill) => {
+        return (
+          <AdminContentBlock
+            key={skill.id}
+            content={skill}
+            editValue={editValue}
+            saveFunction={saveEditedSkill}
+            editAction={editSkill}
+            deleteFunction={deleteSkill}
+          />
+        );
+      })}
+    </>
+  );
 }
--- pages/admin/training-methods/index.tsx
+++ pages/admin/training-methods/index.tsx
@@ -5,89 +5,91 @@ import AdminPageTitle from "../../../components/pages/admin/AdminPageTitle";
 import AdminContentBlock from "../../../components/pages/admin/AdminContentBlock";
 
 export default function TrainingMethods() {
-	const [trainingMethods, setTrainingMethods] = useState<TrainingMethod[]>([]);
-	const [editValue, setEditValue] = useState<TrainingMethod>();
+  const [trainingMethods, setTrainingMethods] = useState<TrainingMethod[]>([]);
+  const [editValue, setEditValue] = useState<TrainingMethod>();
 
-	function getTrainingMethods() {
-		fetch(`${process.env.NEXT_PUBLIC_URL}/data/training-methods`)
-			.then((response) => response.json())
-			.then((data) => setTrainingMethods(data))
-			.catch((rejected) => {
-				console.error(rejected);
-			});
-	}
+  function getTrainingMethods() {
+    fetch(`${process.env.NEXT_PUBLIC_URL}/data/training-methods`)
+      .then((response) => response.json())
+      .then((data) => setTrainingMethods(data))
+      .catch((rejected) => {
+        console.error(rejected);
+      });
+  }
 
-	useEffect(() => {
-		getTrainingMethods();
-	}, []);
+  useEffect(() => {
+    getTrainingMethods();
+  }, []);
 
-	const editTrainingMethod = (id: any) => {
-		setEditValue(
-			trainingMethods.find((trainingMethod) => trainingMethod.id === id)
-		);
-	};
+  const editTrainingMethod = (id: any) => {
+    setEditValue(
+      trainingMethods.find((trainingMethod) => trainingMethod.id === id)
+    );
+  };
 
-	const saveEditedTrainingMethod = (newTrainingMethod: string) => {
-		if (newTrainingMethod === editValue!.name) {
-			setEditValue(undefined);
-			return;
-		}
+  const saveEditedTrainingMethod = (newTrainingMethod: string) => {
+    if (newTrainingMethod === editValue!.name) {
+      setEditValue(undefined);
+      return;
+    }
 
-		const saveEditedTrainingMethod = {
-			id: editValue!.id,
-			name: newTrainingMethod,
-		};
+    const saveEditedTrainingMethod = {
+      id: editValue!.id,
+      name: newTrainingMethod,
+    };
 
-		axios
-			.put(
-				`${process.env.NEXT_PUBLIC_URL}data/training-methods/${editValue!.id}`,
-				saveEditedTrainingMethod
-			)
-			.then(({ data }) => {
-				setEditValue(undefined);
-				const updatedTrainingMethods = trainingMethods.map((trainingMethod) => {
-					return trainingMethod.id === data.id ? data : trainingMethod;
-				});
-				setTrainingMethods(updatedTrainingMethods);
-			})
-			.catch((err) => {
-				console.error(err);
-			});
-	};
+    axios
+      .put(
+        `${process.env.NEXT_PUBLIC_URL}data/training-methods/${editValue!.id}`,
+        saveEditedTrainingMethod
+      )
+      .then(({ data }) => {
+        setEditValue(undefined);
+        const updatedTrainingMethods = trainingMethods.map((trainingMethod) => {
+          return trainingMethod.id === data.id ? data : trainingMethod;
+        });
+        setTrainingMethods(updatedTrainingMethods);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
 
-	const deleteTrainingMethod = (id: any) => {
-		fetch(`${process.env.NEXT_PUBLIC_URL}/data/training-methods/${id}`, {
-			method: "DELETE",
-		})
-			.then(() => {
-				setTrainingMethods(
-					trainingMethods.filter((trainingMethod) => trainingMethod.id != id)
-				);
-			})
-			.catch((rejected) => {
-				console.error(rejected);
-			});
-	};
+  const deleteTrainingMethod = (id: any) => {
+    fetch(`${process.env.NEXT_PUBLIC_URL}/data/training-methods/${id}`, {
+      method: "DELETE",
+    })
+      .then(({ statusText }) => {
+        if (statusText === "OK") {
+          setTrainingMethods(
+            trainingMethods.filter((trainingMethod) => trainingMethod.id != id)
+          );
+        }
+      })
+      .catch((rejected) => {
+        console.error(rejected);
+      });
+  };
 
-	return (
-		<>
-			<AdminPageTitle
-				title="Manage Training Methods"
-				buttonLink="/admin/training-methods/add-training-method"
-				contentLength={trainingMethods.length}
-			/>
-			{trainingMethods.map((trainingMethod) => {
-				return (
-					<AdminContentBlock
-						key={trainingMethod.id}
-						content={trainingMethod}
-						editValue={editValue}
-						editAction={editTrainingMethod}
-						saveFunction={saveEditedTrainingMethod}
-						deleteFunction={deleteTrainingMethod}
-					/>
-				);
-			})}
-		</>
-	);
+  return (
+    <>
+      <AdminPageTitle
+        title="Manage Training Methods"
+        buttonLink="/admin/training-methods/add-training-method"
+        contentLength={trainingMethods.length}
+      />
+      {trainingMethods.map((trainingMethod) => {
+        return (
+          <AdminContentBlock
+            key={trainingMethod.id}
+            content={trainingMethod}
+            editValue={editValue}
+            editAction={editTrainingMethod}
+            saveFunction={saveEditedTrainingMethod}
+            deleteFunction={deleteTrainingMethod}
+          />
+        );
+      })}
+    </>
+  );
 }

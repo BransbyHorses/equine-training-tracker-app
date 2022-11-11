fb7a9e76bc5c7755400c78579e80c1fef0c739ff
michaelthorpe <michael.thorpe@digital.homeoffice.gov.uk>
2022-11-11 10:20:54 +0000

    remove console.log and useEffect dependency

--- utils/hooks/useCollection.ts
+++ utils/hooks/useCollection.ts
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 const useCollection = (
-	endpoint?: string,
+	endpoint: string
 ): {
 	fetchingData: boolean;
 	collection: any[] | [];
@@ -14,23 +14,20 @@ const useCollection = (
 
 	useEffect(() => {
 		setFetchingData(true);
-		if (endpoint) {
-			axios
-				.get(`${process.env.NEXT_PUBLIC_URL}data/${endpoint}`)
-				.then(({ data }) => {
-					console.log(data);
-					setCollection(data);
-					setFetchingData(false);
-				})
-				.catch((err) => {
-					console.error(
-						`Failed to fetch collection. Failed with error message: ${err}.`
-					);
-					setFetchingData(false);
-					setError(true);
-				});
-		}
-	}, [endpoint]);
+		axios
+			.get(`${process.env.NEXT_PUBLIC_URL}data/${endpoint}`)
+			.then(({ data }) => {
+				setCollection(data);
+				setFetchingData(false);
+			})
+			.catch((err) => {
+				console.error(
+					`Failed to fetch collection. Failed with error message: ${err}.`
+				);
+				setFetchingData(false);
+				setError(true);
+			});
+	}, []);
 
 	return {
 		collection,

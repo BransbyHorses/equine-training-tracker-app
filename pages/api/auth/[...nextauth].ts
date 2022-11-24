f6e738939626e5792d4ea1a3a8952e3a860e30af
michaelthorpe <michael.thorpe@digital.homeoffice.gov.uk>
2022-11-24 15:21:47 +0000

    remove console.logs and commented code in [...nextauth] file

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,17 +1,8 @@
 import NextAuth from "next-auth"
-import CognitoProvider from "next-auth/providers/cognito"
 import AzureADProvider from "next-auth/providers/azure-ad";
 import jwt_decode from 'jwt-decode'
-import { ConstructionOutlined } from "@mui/icons-material";
 
 export default NextAuth({
-  // providers: [
-  //   CognitoProvider({
-  //     clientId: process.env.NEXT_PUBLIC_COGNITO_CLIENT_ID!,
-  //     clientSecret: process.env.NEXT_PUBLIC_COGNITO_CLIENT_SECRET!,
-  //     issuer: process.env.NEXT_PUBLIC_COGNITO_ISSUER
-  //   }),
-  // ],
   providers: [
     AzureADProvider({
       clientId: process.env.AZURE_AD_CLIENT_ID!,
@@ -29,7 +20,6 @@ export default NextAuth({
       if (account) {
         token.accessToken = account.access_token;
         let decodedToken: any = jwt_decode(account.access_token!);
-        console.log(decodedToken);
         let wids = decodedToken["wids"] ? decodedToken["wids"][0] : null;
         token.role = wids === '62e90394-69f5-4237-9190-012177145e10' ? 'ADMIN' : null;
         token.sub = decodedToken['sub'];
